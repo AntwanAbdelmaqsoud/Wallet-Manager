673811c98173c6b23a978dbcab8c8c5c44e39d86
Antwan Abdelmaqsoud <antwan0tony@gmail.com>
2025-02-08 19:04:47 +0200

    Added Homepage 🏠

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from "./components/LandingPage";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import UserPage from "./components/UserPage";
+import HomePage from "./components/HomePage";
 import ContactPage from "./components/ContactPage";
 import AboutPage from "./components/AboutPage";
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/:userId" element={<UserPage />} />
---
+++ src/components/HomePage.tsx
@@ -0,0 +1,44 @@
+import Cookies from "js-cookie";
+import SideBar from "./SideBar";
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+  const myCookie = Cookies.get("refreshToken");
+  const [data, setdata] = useState<object | null>(null);
+  useEffect(() => {
+    const getUserData = async (refreshToken: string) => {
+      const data = await fetch(
+        "wallet-manager-api-production.up.railway.app/api/sessions/me",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refreshToken }),
+          credentials: "include",
+        }
+      );
+      const dataAsJson = await data.json();
+      return dataAsJson;
+    };
+    setdata(getUserData(myCookie!));
+  }, [myCookie]);
+
+  return (
+    <div className="min-h-screen flex p-2 bg-[#F6F6F6]">
+      <SideBar />
+      <div className="p-2 text-3xl">
+        HomePage: {myCookie}
+        <br />
+        Data:{" "}
+        {data
+          ? Object.entries(data).map(([key, value]) => (
+              <li key={key}>
+                <strong>{key}:</strong> {value}
+              </li>
+            ))
+          : ""}
+      </div>
+    </div>
+  );
+}
