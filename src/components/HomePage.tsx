bef6bda23455edfeaa1dc618181eb4f8ea324b6b
Antwan Abdelmaqsoud <antwan0tony@gmail.com>
2025-02-08 20:37:03 +0200

    RefreshToken

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,26 +4,17 @@ import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const myCookie = Cookies.get("refreshToken");
-  console.log(myCookie);
   const [data, setdata] = useState<object | null>(null);
 
   useEffect(() => {
     const getUserData = async () => {
       const data = await fetch(
-        "https://wallet-manager-api-production.up.railway.app/api/sessions/me",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
+        "https://wallet-manager-api-production.up.railway.app/api/sessions/me"
       );
       const dataAsJson = await data.json();
       return dataAsJson;
     };
     setdata(getUserData());
-    console.log(myCookie);
   }, []);
 
   return (
